4b3820110c349d02edeeeeda3c240c767da1ff77
JPetta <jovipetraratulangi@gmail.com>
2020-03-20 22:11:39 +0700

    mergin

--- app/App.js
+++ app/App.js
@@ -7,13 +7,20 @@ import TabNavigator from './navigations/HomeTabNavigation'
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native'
 import TabNavigator  from './navigations/HomeTabNavigation';
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 
 export default function App() {
   return (
     <NavigationContainer>
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
       <TabNavigator />
+=======
+      <TabNavigator/>
+>>>>>>> Stashed changes
 =======
       <TabNavigator/>
 >>>>>>> Stashed changes
