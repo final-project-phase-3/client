44df8ad8e595a06236d07df65e2ceb2049ecfedf
JPetta <jovipetraratulangi@gmail.com>
2020-03-21 19:01:11 +0700

    cooking steps 90% done

--- app/components/RecipeDetailsComp.jsx
+++ app/components/RecipeDetailsComp.jsx
@@ -1,41 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text, Image, ImageBackground } from 'react-native'
 import Styles from '../Styles'
-import { ScrollView, TouchableNativeFeedback } from 'react-native-gesture-handler'
+import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler'
+
+import EquipAndIngredients from '../components/EquipAndIngredients'
+import StartCookingButton from '../components/StartCookingButton'
+import CookingStepButton from '../components/CookingStepButton'
+import CookingSteps from '../components/CookingSteps'
 
 export default RecipeDetails = (props) => {
 
+    const [isStarted, setIsStarted] = useState(false)
+
     const { recipe } = props
+    
+    function handleClickStart(){
+        setIsStarted(!isStarted)
+    }
+
+    useEffect(()=>{
+        console.log(isStarted)
+    },[isStarted])
 
     return(
         <ImageBackground source={require('../assets/SearchBackground.png')} style={{width: '100%', height: '100%'}}>
             <View style={{alignItems : 'center', flex : 1}}>
                 <View style={{flex : 0.75, width : '100%', marginBottom : 10}}>
-                    <Text style={{...Styles.TitleText, marginTop : 10}}>{recipe.name}</Text>
+                    <Text style={{...Styles.TitleText, marginTop : 10}}>{recipe.title}</Text>
                 </View>
                 <View style={{flex : 2.5, width : '100%', paddingHorizontal : 10, marginBottom : 10}}>
-                    <Image borderRadius={24} source={{uri : recipe.url}} style={{height : '100%', width : '100%'}}></Image>
+                    <Image borderRadius={24} source={{uri : recipe.image}} style={{height : '100%', width : '100%'}}></Image>
                 </View>
                 <View style={{flex : 4.5, width : '100%', paddingHorizontal : 10}}>
                     <ScrollView>
-                        <View style={{...Styles.Shadows, width : '100%', backgroundColor : 'white', borderRadius : 24, marginVertical : 10, padding : 20}}>
-                            <Text style={{fontSize : 30, fontFamily : 'reem-kufi'}}>Equipments :</Text>
-                            <Text style={{fontSize : 20, fontFamily : 'reem-kufi'}}>{'\t'} {'\u2022'} Pan </Text>
-                            <Text style={{fontSize : 20, fontFamily : 'reem-kufi'}}>{'\t'} {'\u2022'} Pot </Text>
-                            <Text style={{fontSize : 20, fontFamily : 'reem-kufi'}}>{'\t'} {'\u2022'} Spatula </Text>
-                            <Text style={{fontSize : 20, fontFamily : 'reem-kufi'}}>{'\t'} {'\u2022'} Stove </Text>
-                            <Text style={{fontSize : 20, fontFamily : 'reem-kufi'}}>{'\t'}  </Text>
-                            <Text style={{fontSize : 30, fontFamily : 'reem-kufi'}}>Ingredients : </Text>
-                            <Text style={{fontSize : 20, fontFamily : 'reem-kufi'}}>{'\t'} {'\u2022'} Beef</Text>
-                            <Text style={{fontSize : 20, fontFamily : 'reem-kufi'}}>{'\t'} {'\u2022'} Salt</Text>
-                            <Text style={{fontSize : 20, fontFamily : 'reem-kufi'}}>{'\t'} {'\u2022'} Coconut Milk</Text>
-                            <Text style={{fontSize : 20, fontFamily : 'reem-kufi'}}>{'\t'} {'\u2022'} Sweet Soy Sauce</Text>
-                        </View>
-                        <View style={{...Styles.Shadows, width : '100%', backgroundColor : 'white', borderRadius : 24, marginVertical : 10, padding : 20}}>
-                            <TouchableNativeFeedback>
-                                <Text>Start</Text>
-                            </TouchableNativeFeedback>
-                        </View>
+                        
+                        { !isStarted &&
+                            <>
+                                <EquipAndIngredients recipe={ recipe }/>
+                                <StartCookingButton handleClickStart={handleClickStart}/>
+                            </>
+                        }
+                        { isStarted &&
+                            <>
+                                <CookingSteps recipe={ recipe }/>
+                                <CookingStepButton handleClickStart={handleClickStart}/>
+                            </>
+                        }
+                        
                     </ScrollView>
                 </View>
                 
---
+++ app/recipesMockup.js
@@ -0,0 +1,286 @@
+export default ()=>{
+    return (
+        [
+            {
+                title: 'Beef Rendang',
+                ingredients : [
+                    { 
+                        name : 'Beef',
+                        amount : '1kgs'
+                    },
+                    { 
+                        name : 'Coconut Milk',
+                        amount : '2 cup'
+                    },
+                    { 
+                        name : 'Salt',
+                        amount : '1 tbsp'
+                    },
+                    { 
+                        name : 'Oil',
+                        amount : '1/2 lt'
+                    },
+                ],
+                equipments : [
+                    { 
+                        name : 'Pan',
+                        amount : '1 pc/s'
+                    },
+                    { 
+                        name : 'Pot',
+                        amount : '1 pc/s'
+                    },
+                    { 
+                        name : 'Spatula',
+                        amount : '1 pc/s'
+                    },
+                    { 
+                        name : 'Stove',
+                        amount : '1 pc/s'
+                    },
+                ],
+                image:'https://s3media.freemalaysiatoday.com/wp-content/uploads/2019/06/rendang-lifestyle-020619-1.jpg'
+            },
+            {
+                title: 'Chicken Opor',
+                ingredients : [
+                    { 
+                        name : 'Chicken',
+                        amount : '1kgs'
+                    },
+                    { 
+                        name : 'Coconut Milk',
+                        amount : '2 cup'
+                    },
+                    { 
+                        name : 'Salt',
+                        amount : '1 tbsp'
+                    },
+                    { 
+                        name : 'Oil',
+                        amount : '1/2 lt'
+                    },
+                ],
+                equipments : [
+                    { 
+                        name : 'Pan',
+                        amount : '1 pc/s'
+                    },
+                    { 
+                        name : 'Pot',
+                        amount : '1 pc/s'
+                    },
+                    { 
+                        name : 'Spatula',
+                        amount : '1 pc/s'
+                    },
+                    { 
+                        name : 'Stove',
+                        amount : '1 pc/s'
+                    },
+                ],
+                image:'https://cdn2.tstatic.net/palu/foto/bank/images/opor-ayam-lezat-untuk-sajian-lebaran.jpg'
+            },
+            {
+                title: 'Spaghetti Bolognese',
+                ingredients : [
+                    { 
+                        name : 'Ground Beef',
+                        amount : '1kgs'
+                    },
+                    { 
+                        name : 'Milk',
+                        amount : '2 cup'
+                    },
+                    { 
+                        name : 'Salt',
+                        amount : '1 tbsp'
+                    },
+                    { 
+                        name : 'Oil',
+                        amount : '1/2 lt'
+                    },
+                ],
+                equipments : [
+                    { 
+                        name : 'Pan',
+                        amount : '1 pc/s'
+                    },
+                    { 
+                        name : 'Pot',
+                        amount : '1 pc/s'
+                    },
+                    { 
+                        name : 'Spatula',
+                        amount : '1 pc/s'
+                    },
+                    { 
+                        name : 'Stove',
+                        amount : '1 pc/s'
+                    },
+                ],
+                image:'https://thecozyapron.com/wp-content/uploads/2019/05/spaghetti-bolognese_thecozyapron_1.jpg'
+            },
+            {
+                title: 'Sayur Asem',
+                ingredients : [
+                    { 
+                        name : 'Corn',
+                        amount : '1kgs'
+                    },
+                    { 
+                        name : 'Pumpkin',
+                        amount : '2 cup'
+                    },
+                    { 
+                        name : 'Salt',
+                        amount : '1 tbsp'
+                    },
+                    { 
+                        name : 'Oil',
+                        amount : '1/2 lt'
+                    },
+                ],
+                equipments : [
+                    { 
+                        name : 'Pan',
+                        amount : '1 pc/s'
+                    },
+                    { 
+                        name : 'Pot',
+                        amount : '1 pc/s'
+                    },
+                    { 
+                        name : 'Spatula',
+                        amount : '1 pc/s'
+                    },
+                    { 
+                        name : 'Stove',
+                        amount : '1 pc/s'
+                    },
+                ],
+                image:'https://cdns.klimg.com/merdeka.com/i/w/news/2018/04/25/968568/670x335/5-cara-masak-sayur-asem-yang-enak-segar-dan-mudah-ala-sunda-betawi-serta-jawa-rev-1.jpg'
+            },
+            {
+                title: 'Sweet And Sour Prawns',
+                ingredients : [
+                    { 
+                        name : 'Prawn',
+                        amount : '1kgs'
+                    },
+                    { 
+                        name : 'Pineapple',
+                        amount : '2 cup'
+                    },
+                    { 
+                        name : 'Salt',
+                        amount : '1 tbsp'
+                    },
+                    { 
+                        name : 'Oil',
+                        amount : '1/2 lt'
+                    },
+                ],
+                equipments : [
+                    { 
+                        name : 'Pan',
+                        amount : '1 pc/s'
+                    },
+                    { 
+                        name : 'Pot',
+                        amount : '1 pc/s'
+                    },
+                    { 
+                        name : 'Spatula',
+                        amount : '1 pc/s'
+                    },
+                    { 
+                        name : 'Stove',
+                        amount : '1 pc/s'
+                    },
+                ],
+                image:'https://oversixtydev.blob.core.windows.net/media/10108/sweet-and-sour-prawns.jpg'
+            },
+            {
+                title: 'Fried Rice',
+                ingredients : [
+                    { 
+                        name : 'Rice',
+                        amount : '1kgs'
+                    },
+                    { 
+                        name : 'Coconut Milk',
+                        amount : '2 cup'
+                    },
+                    { 
+                        name : 'Soy Sauce',
+                        amount : '1 tbsp'
+                    },
+                    { 
+                        name : 'Oil',
+                        amount : '1/2 lt'
+                    },
+                ],
+                equipments : [
+                    { 
+                        name : 'Pan',
+                        amount : '1 pc/s'
+                    },
+                    { 
+                        name : 'Pot',
+                        amount : '1 pc/s'
+                    },
+                    { 
+                        name : 'Spatula',
+                        amount : '1 pc/s'
+                    },
+                    { 
+                        name : 'Stove',
+                        amount : '1 pc/s'
+                    },
+                ],
+                image:'https://thewoksoflife.com/wp-content/uploads/2019/08/fried-brown-rice-12-500x500.jpg'
+            },
+            {
+                title: 'Pan Seared Steak',
+                ingredients : [
+                    { 
+                        name : 'Beef',
+                        amount : '1kgs'
+                    },
+                    { 
+                        name : 'Pepper',
+                        amount : '2 cup'
+                    },
+                    { 
+                        name : 'Salt',
+                        amount : '1 tbsp'
+                    },
+                    { 
+                        name : 'Oil',
+                        amount : '1/2 lt'
+                    },
+                ],
+                equipments : [
+                    { 
+                        name : 'Pan',
+                        amount : '1 pc/s'
+                    },
+                    { 
+                        name : 'Pot',
+                        amount : '1 pc/s'
+                    },
+                    { 
+                        name : 'Spatula',
+                        amount : '1 pc/s'
+                    },
+                    { 
+                        name : 'Stove',
+                        amount : '1 pc/s'
+                    },
+                ],
+                image:'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2007/1/2/0/valentines_steak.jpg.rend.hgtvcom.826.620.suffix/1557859049553.jpeg'
+            }
+        ]
+    )
+}
