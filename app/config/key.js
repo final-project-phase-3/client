ddef68270216f5fe6b1cf5cdce50a401a3a885dc
jetly sandita <jetly27sandita@gmail.com>
2020-03-25 12:53:11 +0700

    login register

--- app/config/key.js
+++ app/config/key.js
@@ -1,5 +1,13 @@
+import {AsyncStorage} from 'react-native';
 export default {
   BASE_URL: 'http://192.168.100.48',
-  devToken:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlNzQ4MWUzMTJhZjMyNGU2YTRjZTQwNiIsImlhdCI6MTU4NDcwODI5N30.bJjQX2-RTd8EvKl5_f-oorQyt8ucmB2xcqK_R1TuhTg'
+  devToken: async () => {
+    try {
+      const value = await AsyncStorage.getItem('token')
+      return value
+    } catch (error) {
+      return null
+    }
+  }
+    // 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlNzQ4MWUzMTJhZjMyNGU2YTRjZTQwNiIsImlhdCI6MTU4NDcwODI5N30.bJjQX2-RTd8EvKl5_f-oorQyt8ucmB2xcqK_R1TuhTg'
 }
